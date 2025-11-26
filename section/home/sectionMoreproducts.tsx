8b90c459daef14b6689a68164eeb497f8fa45a1c
erlandel <erlandelfino@gmail.com>
2025-11-26 18:18:11 -0500

    Improving the responsiveness of the sectionMoreproducts

--- section/home/sectionMoreproducts.tsx
+++ section/home/sectionMoreproducts.tsx
@@ -17,20 +17,12 @@ export default function SectionMoreproducts() {
         </span>
       </div>
 
-      <div className="flex flex-col xl:flex-row justify-center items-center  ">
+      <div className="flex flex-col mx-3 xl:flex-row justify-center items-center  ">
         <div
           id="card1"
-          className="mx-1 lg:w-6/12 relative overflow-hidden rounded-xl"
+          className=" xl:w-6/12  sm:mx-15 xl:mx-0  relative overflow-hidden rounded-xl"
         >
-          <div>
-            <img
-              src="/images/vent.png"
-              alt="vent"
-              className="w-70 h-50 ml-25 xl:w-1/2 xl:h-1/2 object-cover  lg:ml-90 mt-4"
-            />
-          </div>
-
-          <div className="absolute inset-0 z-10 flex flex-col justify-center p-2 mr-50 lg:ml-32 w-55 lg:w-82">
+          <div className="absolute inset-0 z-10 flex flex-col justify-center sm:p-2 xl:ml-32 w-55 sm:w-70 lg:w-80 xl:w-70  2xl:w-82">
             <div className="text-black ">
               <h4 className="font-bold text-xl">Ventilador</h4>
               <p className="text-xs w-42 sm:text-base sm:w-full">
@@ -41,19 +33,26 @@ export default function SectionMoreproducts() {
                 <ButtonPromoHome1
                   name="Ver más"
                   color="#022954"
-                  icon={<ArrowRight  className="w-4 h-4 md:w-5 md:h-5" />}
+                  icon={<ArrowRight className="w-4 h-4 md:w-5 md:h-5" />}
                   onClick={() => router.push("/products")}
                 />
               </div>
             </div>
           </div>
+          <div>
+            <img
+              src="/images/vent.png"
+              alt="vent"
+              className="w-70 h-55 ml-25 mt-4 sm:w-90 sm:h-70 sm:ml-50  md:h-90 md:w-120   xl:ml-82 2xl:ml-90 xl:w-90  2xl:w-1/2 xl:h-1/2 object-cover "
+            />
+          </div>
         </div>
 
         <div
           id="card2"
-          className=" sm:w-6/12 flex  justify-center items-center relative"
+          className=" xl:w-6/12 flex sm:mx-15 xl:mx-0  justify-center items-center relative"
         >
-          <div className="absolute mr-55  sm:mr-60  z-50 w-45 sm:w-80 p-4 text-black">
+          <div className="absolute left-[-10]  xl:left-auto xl:mr-60 2xl:mr-60  z-50 w-50 sm:w-70 md:w-80 p-4 text-black">
             <h4 className="font-bold text-xl">Refrigeradores y Neveras</h4>
             <p className="text-xs sm:text-base ">
               Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit
@@ -64,16 +63,17 @@ export default function SectionMoreproducts() {
               <ButtonPromoHome1
                 name="Ver más"
                 color="#022954"
-                icon={<ArrowRight  className="w-4 h-4 md:w-5 md:h-5" />}
+                icon={<ArrowRight className="w-4 h-4 md:w-5 md:h-5" />}
                 onClick={() => router.push("/products")}
               />
             </div>
           </div>
+
           <div className="mt-5">
             <img
               src="/images/Fridge.png"
               alt="vent"
-              className="ml-4 sm:ml-4 sm:h-70 object-contain xl:ml-50"
+              className="ml-8 sm:ml-10    xl:h-70   object-contain xl:ml-30"
             />
           </div>
         </div>
